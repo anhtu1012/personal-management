ddfe0ce1db4304f2c247e48c677a547e677e3c46
anhtu1012 <phamanhtu1012@gmail.com>
2026-03-28 22:20:53 +0700

    fixx

--- store/index.ts
+++ store/index.ts
@@ -3,7 +3,7 @@ import taskReducer from "./slices/taskSlice"
 import themeReducer from "./slices/themeSlice"
 import { localStorageMiddleware } from "./middleware/localStorageMiddleware"
 
-export const makeStore = () => {
+const makeStore = () => {
   return configureStore({
     reducer: {
       tasks: taskReducer,
@@ -16,5 +16,6 @@ export const makeStore = () => {
 
 export const store = makeStore()
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore["getState"]>
+export type AppDispatch = AppStore["dispatch"]
